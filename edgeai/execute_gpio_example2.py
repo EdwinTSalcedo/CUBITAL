ecc7dca241042156c6958cbfc0fc283744e56706
Edwin Salcedo <ingedwinsalcedo@gmail.com>
2024-02-26 11:53:38 -0400

    Added examples

---
+++ edgeai/execute_gpio_example2.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+# Use Broadcom SOC Pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Pin to be used for LED/PWM
+pin = 12
+
+# Set up the GPIO pin as an output
+GPIO.setup(pin, GPIO.OUT)
+
+try:
+   # Turn the pin on
+   GPIO.output(pin, GPIO.HIGH)
+   print("Pin 12 is ON")
+   time.sleep(20)  # Wait for 20 seconds
+
+   # Turn the pin off
+   GPIO.output(pin, GPIO.LOW)
+   print("Pin 12 is OFF")
+   time.sleep(3)  # Wait for 3 seconds
+
+finally:
+    # Clean up the GPIO to reset pin configuration
+    GPIO.cleanup()
